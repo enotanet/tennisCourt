3627f644aa8ddbf7ce0f8502448b5c1a0a12a675
Yordan Chaparov <yc8412@line13.doc.ic.ac.uk>
2014-10-24 11:40:11 +0100

    Fixing OpenCV code to work with C++ API. Should be better now

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 CFLAGS = `pkg-config --cflags opencv`
 LIBS = `pkg-config --libs opencv`
 
-simple.o: simple.cpp
-	g++ $(CFLAGS) -c -o simple.o simple.cpp
 all: simple.o
-	g++ $(LIBS) -g -o binary  simple.o
+	g++ -o binary  simple.o $(LIBS)
+simple: simple.cpp
+	g++ $(CFLAGS) -c -o simple.o simple.cpp
 clean:
 	rm -rf *o binary
--- simple.cpp
+++ simple.cpp
@@ -1,13 +1,26 @@
-#include "cv.h"      // include it to used Main OpenCV functions.
-#include "highgui.h" //include it to use GUI functions.
+#include <iostream>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/core/core.hpp"
+
+using namespace cv;
+using namespace std;
+
+int main( int argc, const char** argv )
+{
+    cout << "Starting up!\n";
+    Mat img = imread("fwm-small.jpg", CV_LOAD_IMAGE_COLOR);
+
+    if (img.empty()) //check whether the image is loaded or not
+    {
+        cout << "Error : Image cannot be loaded..!!" << endl;
+                  return -1;
+    }
+
+    namedWindow("MyWindow", CV_WINDOW_AUTOSIZE); //create a window with the name "MyWindow"
+    imshow("MyWindow", img); //display the image which is stored in the 'img' in the "MyWindow" window
  
- int main(int argc, char** argv)
- {
-         IplImage* img = cvLoadImage( "fwm-small.jpg" ); //change the name (image.jpg) according to your Image filename.
-         cvNamedWindow( "Example1", CV_WINDOW_AUTOSIZE );
-         cvShowImage("Example1", img);
-         cvWaitKey(0);
-         cvReleaseImage( &img );
-         cvDestroyWindow( "Example1" );
-         return 0;
- }
+    waitKey(0);
+    destroyWindow("MyWindow"); //destroy the window with the name, "MyWindow"
+
+    return 0;
+}
