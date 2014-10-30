f74c78ef214326530823a18049b3344dcf7fe3e0
Maciek <maciek@maciek-Dell-XPS-L502X.(none)>
2014-10-30 17:31:12 +0000

    new simple.cpp, different openCV API used, changed Makefile, works on MY machine

--- simple.cpp
+++ simple.cpp
@@ -1,13 +1,26 @@
-#include "cv.h"      // include it to used Main OpenCV functions.
-#include "highgui.h" //include it to use GUI functions.
- 
- int main(int argc, char** argv)
- {
-         IplImage* img = cvLoadImage( "fwm-small.jpg" ); //change the name (image.jpg) according to your Image filename.
-         cvNamedWindow( "Example1", CV_WINDOW_AUTOSIZE );
-         cvShowImage("Example1", img);
-         cvWaitKey(0);
-         cvReleaseImage( &img );
-         cvDestroyWindow( "Example1" );
-         return 0;
- }
+#include "opencv2/highgui/highgui.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main( int argc, const char** argv )
+{
+  Mat img(500, 1000, CV_8UC3, Scalar(0,0, 100)); //create an image ( 3 channels, 8 bit image depth, 500 high, 1000 wide, (0, 0, 100) assigned for Blue, Green and Red plane respectively. )
+
+  if (img.empty()) //check whether the image is loaded or not
+  {
+    cout << "Error : Image cannot be loaded..!!" << endl;
+    system("pause"); //wait for a key press
+    return -1;
+  }
+  
+  namedWindow("MyWindow", CV_WINDOW_AUTOSIZE); //create a window with the name "MyWindow"
+  //imshow("MyWindow", img); //display the image which is stored in the 'img' in the "MyWindow" window
+  
+  waitKey(0);  //wait infinite time for a keypress
+  
+  destroyWindow("MyWindow"); //destroy the window with the name, "MyWindow"
+  
+  return 0;
+}
