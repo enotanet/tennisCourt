bd96eef318fdcf975f908728d26000bda0522d55
ybulgakova <yulia_bulgakova@hotmail.com>
2014-12-05 12:53:23 +0000

    Created folder for computer vision utilities. Added a method for cutting reange of intesities of an image.

---
+++ TennisCourt/TennisCourt/CV_Utils/preprocessing.h
@@ -0,0 +1,8 @@
+#ifndef PREPROCESSING_H__
+#define PREPROCESSING_H__
+#include "opencv2/imgproc/imgproc.hpp"
+#include <vector>
+
+void cutBetweenIntensities(cv::Mat &image, int lowerBound, int upperBound, bool inverse=false);
+
+#endif
