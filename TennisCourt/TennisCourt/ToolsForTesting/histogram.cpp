faac2f8e35c2e09a26bb3a81ddf42ca22675c9e0
ybulgakova <yulia_bulgakova@hotmail.com>
2014-12-07 23:39:40 +0000

    Add an option of highlighting values on the histogram

--- TennisCourt/TennisCourt/ToolsForTesting/histogram.cpp
+++ TennisCourt/TennisCourt/ToolsForTesting/histogram.cpp
@@ -1,4 +1,5 @@
 #include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 
 #include <cstdlib>
 
@@ -7,16 +8,33 @@
 using namespace std;
 using namespace cv;
 
+int histogramWidth = 512; 
+int histogramHeight = 400;
+int lineThickness = cvRound((double) histogramWidth/256);
+
 /**
  * This method displays the histogram for the given image. 
  * Parameters:
  * Mat &image - reference to an image for which histogram is displayed.
  * string windowName - name of the window in which histogram will be displayed.
+ * (optional) vector<int> intensitiesToHighlight - values of intensities at which to draw a vertical red line
  */
-void displayHistogram(Mat &image, string windowName) {
+void displayHistogram(Mat &image, string windowName, vector<int> &intensitiesToHighlight) {
   // Display histogram in a new window
+  Mat histogram = drawHistogram(image);
+  cvtColor(histogram, histogram, CV_GRAY2RGBA);
+
+  if (!intensitiesToHighlight.empty()) {
+    for (int intensity : intensitiesToHighlight) {
+      line(histogram, 
+           Point(lineThickness * intensity, histogramHeight), 
+           Point(lineThickness * intensity, 0), 
+           Scalar(0,0,255), 1, 8, 0);
+    }
+  }
+
   namedWindow(windowName, CV_WINDOW_AUTOSIZE); 
-  imshow(windowName, drawHistogram(image));
+  imshow(windowName, histogram);
 }
 
 /**
@@ -37,14 +55,9 @@ Mat drawHistogram(Mat &image) {
       intensities[(int)image.at<uchar>(y,x)]++;
     }
   }
-
-  int histogramWidth = 512; 
-  int histogramHeight = 400;
-  int lineThickness = cvRound((double) histogramWidth/256);
-
   // Histogram to be displayed
   Mat histogram(histogramHeight, histogramWidth, CV_8UC1, Scalar(255, 255, 255));
-  
+
   // Find the maximum intensity element from histogram
   int max = intensities[0];
   for(int i = 1; i < 256; i++)
@@ -68,5 +81,4 @@ Mat drawHistogram(Mat &image) {
   
   return histogram;
 }
-
 
