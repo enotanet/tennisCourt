ef4f38755b299ead0a359f4a0a78b2eab565765b
Yordan <yc8412@ic.ac.uk>
2015-01-05 23:30:08 +0200

    2d ball trajectory prediction

--- TennisCourt/TennisCourt/analysis_system.h
+++ TennisCourt/TennisCourt/analysis_system.h
@@ -4,8 +4,59 @@
 #include "camera_location.h"
 #include "sys_frame_grabber.h"
 #include "ballFinder.h"
+#include "utils.h"
 #include <opencv2/core/core.hpp>
 #include <vector>
+#include <set>
+
+// This is getting clumsy and memory-heavy.
+//
+struct ParabolaTraj2d {
+  double xa;
+  double xb;
+  double xc;
+
+  double ya;
+  double yb;
+  double yc;
+
+  int first;
+  int last;
+
+  cv::Point2d evaluate(double t) const {
+    return cv::Point2d(xa * t * t + xb * t + xc, ya * t * t + yb * t + yc);
+  }
+};
+
+class LinkedParabolaTrajectory2d {
+public:
+  LinkedParabolaTrajectory2d(
+      const std::vector<std::pair<int, cv::Point2f>> &points,
+      const std::vector<ParabolaTraj2d> &trajectories);
+  bool EvaluateAt(double t, cv::Point2d *p);
+private:
+  double ParabolaDistance(const std::vector<ParabolaTraj2d> &trajectories, int a, int b);
+
+  // Satisfied sets for all trajectories. Expensive!
+  //
+  std::vector<std::set<int>> satisfied;
+  std::vector<ParabolaTraj2d> path;
+};
+
+struct ParabolaTraj {
+  double a;
+  double b;
+  double c;
+
+  // First and last point indexes used to approximate the parabola.
+  //
+  int first;
+  int last;
+  
+  // Index of the initial point used to start the trajectory calculation.
+  //
+  int initial;
+};
 
 void RunOnlineSystem(SystemFrameGrabber *grabber);
 
@@ -35,7 +86,11 @@ private:
 
 class FrameProcessor {
 public:
-  FrameProcessor(size_t frame_number) : ballFinders(frame_number), cameraLocations(frame_number), count(0), oa(-1) {}
+  FrameProcessor(size_t frame_number) : ballFinders(frame_number),
+                                        cameraLocations(frame_number),
+                                        ballPositions2d(frame_number),
+                                        count(0),
+                                        oa(-1) {}
   // Maybe bools?
   // Needs state! Wrap in a class.
   //
@@ -44,16 +99,42 @@ public:
 private:
   std::vector<BallFinder> ballFinders;
   std::vector<CameraLocation> cameraLocations;
-  std::vector<std::pair<int, cv::Point2f>> ballPositions;
+  std::vector<std::vector<std::pair<int, cv::Point2f>>> ballPositions2d;
   int count;
   double oa, ob, oc;
+  ParabolaTraj2d parab;
 };
+
+bool MatchXYTrajectories(const std::vector<std::pair<int, cv::Point2f>> &xtime,
+                         const std::vector<ParabolaTraj> &xtrajectories,
+                         const std::vector<std::pair<int, cv::Point2f>> &ytime,
+                         const std::vector<ParabolaTraj> &ytrajectories,
+                         std::vector<ParabolaTraj2d> *trajectories);
+
+bool ParabolaSatisfiedSet(const std::vector<std::pair<int, cv::Point2f>> &ballPositions,
+                          const int min_index,
+                          const int max_index,
+                          const ParabolaTraj &parab,
+                          const double trajectory_eps,
+                          std::vector<int> *satisfied,
+                          double *dist_sum);
+
+double PointToParabolaDistance(cv::Point2d p, const ParabolaTraj &parab) {
+  return PointToParabolaDistance(p, parab.a, parab.b, parab.c);
+}
+
+bool getParabola(std::vector<cv::Point2d> points, ParabolaTraj* parab) {
+  return getParabola(points, &parab->a, &parab->b, &parab->c);
+}
 
 // Approximates the trajectory of a point moving in 2d space with a parabola.
 // This could be a projection of the ball inside a plane.
 //
 void BestTrajectories(const std::vector<std::pair<int, cv::Point2f>> &ballPositions,
-                      std::vector<cv::Point3d> *trajectories);
+                      std::vector<ParabolaTraj> *trajectories,
+                      int max_window_size = 24,
+                      int good_trajectory_threshold = 18,
+                      double trajectory_eps = 1);
 
 void InitialiseOutput(size_t windowCount);
 
